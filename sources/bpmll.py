bdb0b5eb16b2647360c2596123a87256189bb511
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-09-15 14:21:07 +0200

    Add bpmll.py from Lukas Huwald Github repo

---
+++ sources/bpmll.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2017 Lukas Huwald
+
+# You are free to use or modify this implementation for research and non-commercial purposes, as long as you include this license file.
+
+# There is no warranty of any kind. The author is in no way responsible for any
+# consequences resulting of the use of this implementation.
+
+# Adapted from: "Zhang, Min-Ling, and Zhi-Hua Zhou. "Multilabel neural networks
+# with applications to functional genomics and text categorization." IEEE
+# transactions on Knowledge and Data Engineering 18.10 (2006): 1338-1351."
+
+import numpy as np
+import tensorflow as tf
+
+# bp mll loss function
+# y_true, y_pred must be 2D tensors of shape (batch dimension, number of labels)
+# y_true must satisfy y_true[i][j] == 1 iff sample i has label j
+def bp_mll_loss(y_true, y_pred):
+
+    # get true and false labels
+    shape = tf.shape(y_true)
+    y_i = tf.equal(y_true, tf.ones(shape))
+    y_i_bar = tf.not_equal(y_true, tf.ones(shape))
+
+    # get indices to check
+    truth_matrix = tf.to_float(pairwise_and(y_i, y_i_bar))
+
+    # calculate all exp'd differences
+    sub_matrix = pairwise_sub(y_pred, y_pred)
+    exp_matrix = tf.exp(tf.negative(sub_matrix))
+
+    # check which differences to consider and sum them
+    sparse_matrix = tf.multiply(exp_matrix, truth_matrix)
+    sums = tf.reduce_sum(sparse_matrix, axis=[1,2])
+
+    # get normalizing terms and apply them
+    y_i_sizes = tf.reduce_sum(tf.to_float(y_i), axis=1)
+    y_i_bar_sizes = tf.reduce_sum(tf.to_float(y_i_bar), axis=1)
+    normalizers = tf.multiply(y_i_sizes, y_i_bar_sizes)
+    results = tf.divide(sums, normalizers)
+
+    # sum over samples
+    return tf.reduce_sum(results)
+
+# compute pairwise differences between elements of the tensors a and b
+def pairwise_sub(a, b):
+    column = tf.expand_dims(a, 2)
+    row = tf.expand_dims(b, 1)
+    return tf.subtract(column, row)
+
+# compute pairwise logical and between elements of the tensors a and b
+def pairwise_and(a, b):
+    column = tf.expand_dims(a, 2)
+    row = tf.expand_dims(b, 1)
+    return tf.logical_and(column, row)
