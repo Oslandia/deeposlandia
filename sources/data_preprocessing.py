552dca86acff42f28f1069b4473ce8fb2468bd6a
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-09-19 09:35:28 +0200

    clean data_preprocessing.py module

--- sources/data_preprocessing.py
+++ sources/data_preprocessing.py
@@ -9,13 +9,6 @@ import sys
 
 import utils
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-format = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-ch = logging.StreamHandler(sys.stdout)
-ch.setFormatter(format)
-logger.addHandler(ch)
-        
 ##################
 # Transform the image into input data
 # Input data: np.array of shape [2448, 3264, 3]
@@ -28,23 +21,23 @@ logger.addHandler(ch)
 # np.array of shape [18000, 66] for training set, and [2000, 66] for
 # validation set
 
-logger.info("Read config file...")
+utils.logger.info("Read config file...")
 # read in config file
 with open('data/config.json') as config_file:
     config = json.load(config_file)
 labels = config['labels']
 
-# logger.info("Mapillary image size inventory...")
-# utils.mapillary_image_sizes = size_inventory()
-# utils.mapillary_image_sizes.to_csv("data/mapillary_image_sizes.csv")
-# utils.mapillary_image_size_plot(mapillary_image_sizes,
-#                           "../images/mapillary_image_sizes.png")
+utils.logger.info("Mapillary image size inventory...")
+utils.mapillary_image_sizes = size_inventory()
+utils.mapillary_image_sizes.to_csv("data/mapillary_image_sizes.csv")
+utils.mapillary_image_size_plot(mapillary_image_sizes,
+                          "../images/mapillary_image_sizes.png")
 
-logger.info("Mapillary data preparation...")
+utils.logger.info("Mapillary data preparation...")
 utils.mapillary_data_preparation("training", len(labels))
 utils.mapillary_data_preparation("validation", len(labels))
 utils.mapillary_data_preparation("testing", len(labels))
 
-logger.info("Mapillary output checking...")
+utils.logger.info("Mapillary output checking...")
 utils.mapillary_output_checking("training", len(labels))
 utils.mapillary_output_checking("validation", len(labels))
