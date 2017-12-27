4469bc6d1e6a02efd396a6f15f3fe49e55390672
Raphael Delhome <raphael.delhome@oslandia.com>
2017-12-27 16:01:33 +0100

    Build a new dataset-related class in a dedicated module

---
+++ sources/dataset.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+/**
+ *   Raphael Delhome - december 2017
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>
+ */
+"""
+
+class Dataset(object):
+
+    def __init__(self, image_size, glossary_filename):
+        """
+        """
+        self._image_size = image_size
+        self.class_info = defaultdict()
+        self.build_glossary(glossary_filename)
+        self.image_info = defaultdict()
+
+    def get_class(self, class_id):
+        """
+        """
+        if not class_id in self.class_info.keys():
+            print("Class {} not in the dataset glossary".format(class_id))
+            return None
+        return self.class_info[class_id]
+    
+    def get_image(self, image_id):
+        """
+        """
+        if not image_id in self.image_info.keys():
+            print("Image {} not in the dataset".format(image_id))
+            return None
+        return self.image_info[image_id]
+
+    def get_nb_class(self):
+        """
+        """
+        return len(self.class_info)
