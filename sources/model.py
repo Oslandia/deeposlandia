b39594d1539464b67e4c9f6223bd697be54141b5
Raphael Delhome <raphael.delhome@oslandia.com>
2017-12-27 17:03:45 +0100

    Add methods to build basic neural network layers

--- sources/model.py
+++ sources/model.py
@@ -35,4 +35,95 @@ class ConvolutionalNeuralNetwork(object):
     
     def get_num_labels(self):
         return _num_labels
+
+    def convolutional_layer(self, counter, network_name, input_layer,
+                            input_layer_depth, kernel_dim,
+                            layer_depth, strides=[1, 1, 1, 1], padding='SAME'):
+        """Build a convolutional layer as a Tensorflow object,
+        for a convolutional neural network
+
+        Parameters
+        ----------
+        input_layer: tensor
+            input tensor, i.e. the placeholder that represents input data if
+        the convolutional layer is the first of the network, the previous layer
+        output otherwise
+        input_layer_depth: integer
+            input tensor channel number (3 for RGB images, more if another
+        convolutional layer precedes the current one)
+        kernel_dim: integer
+            Dimension of the convolution kernel (only the first one, the kernel
+        being considered as squared; and its last dimensions being given by
+        previous and current layer depths)
+        layer_depth: integer
+            current layer channel number
+        conv_strides: list
+            Dimensions of the convolution stride operation defined as [1,a,a,1]
+        where a is the shift (in pixels) between each convolution operation
+        counter: integer
+            Convolutional layer counter (for scope name unicity)
+        network_name: object
+            string designing the network name (for scope name unicity)
+        """
+        with tf.variable_scope(network_name+'_conv'+str(counter)) as scope:
+            w = self.create_weights([kernel_dim, kernel_dim,
+                                     input_layer_depth, layer_depth])
+            b = self.create_biases([layer_depth])
+            conv = tf.nn.conv2d(input_layer, w, strides=strides,
+                                padding=padding)
+            return tf.nn.relu(tf.add(conv, b), name=scope.name)
+
     
+    def maxpooling_layer(self, counter, network_name, input_layer, kernel_dim,
+                         stride=2, padding='SAME'):
+        """Build a max pooling layer as a Tensorflow object,
+        into the convolutional neural network
+
+        Parameters
+        ----------
+        counter: integer
+            Max pooling layer counter (for scope name unicity)
+        network_name: object
+            string designing the network name (for scope name unicity)
+        input_layer: tensor
+            Pooling layer input; output of the previous layer into the network
+        kernel_dim: list
+            Dimension of the pooling kernel, defined as [1, a, a, 1]
+        where a is the main kernel dimension (in pixels)
+        stride: list
+            Dimension of the pooling stride, defined as [1, a, a, 1]
+        where a is the shift between each pooling operation
+
+        """
+        with tf.variable_scope(network_name + '_pool' + str(counter)) as scope:
+            return tf.nn.max_pool(input_layer,
+                                  ksize=[1, kernel_dim, kernel_dim, 1],
+                                  strides=[1,stride,stride,1], padding=padding)
+
+    def fullyconnected_layer(self, counter, network_name, input_layer,
+                             last_layer_dim, layer_depth, t_dropout=1.0):
+        """Build a fully-connected layer as a tensor, into the convolutional
+                       neural network
+
+        Parameters
+        ----------
+        input_layer: tensor
+            Fully-connected layer input; output of the previous layer into the network
+        last_layer_dim: integer
+            previous layer depth, into the network
+        layer_depth: integer
+            full-connected layer depth
+        t_dropout: tensor
+            tensor corresponding to the neuron keeping probability during dropout operation
+        counter: integer
+            fully-connected layer counter (for scope name unicity)
+        network_name: object
+            string designing the network name (for scope name unicity)
+
+        """
+        with tf.variable_scope(network_name + '_fc' + str(counter)) as scope:
+            reshaped = tf.reshape(input_layer, [-1, last_layer_dim])
+            w = self.create_weights([last_layer_dim, layer_depth])
+            b = self.create_biases([layer_depth])
+            return tf.nn.relu(tf.add(tf.matmul(reshaped, w), b), name='relu')
+            # return tf.nn.dropout(fc, t_dropout, name='relu_with_dropout')
