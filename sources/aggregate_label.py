66bc6951d869d5272833c0640e9dff3d9a748aaa
Damien Garaud <damien.garaud@oslandia.com>
2018-03-05 12:07:12 +0100

    update the aggregate label script
    Just generate a new JSON config file.

--- sources/aggregate_label.py
+++ sources/aggregate_label.py
@@ -1,27 +1,15 @@
-"""Image resizing and aggregation labels
-
-Can pass this script with the GNU command 'parallel' in order to process the
-images in parallel. Let image you have a text file `image.list`:
-
-cat image.list | parallel "python aggregate_label.py -s=256 -k={}"
-
-Or alternatively, without this text file:
-
-ls <image_repository> | sed -e 's/\.jpg$//' | parallel "python aggregate_label.py -s=256 -k={}"
+"""Script to aggregate a few Mapillary labels
 """
 
 import os
 import json
 
-import numpy as np
 import pandas as pd
-
-from PIL import Image
-
-import utils
 
 
 DATASET = 'mapillary'
+# column name which contains the name of the aggregated label family
+KEY_AGG = 'agg_label'
 
 
 def _split_object_label(df):
@@ -42,9 +30,9 @@ def _split_object_label(df):
     """
     df = df.copy()
     mask_vehicle = df['name'].str.split('--').apply(lambda x: 'vehicle' in x)
-    df.loc[mask_vehicle, 'group_label'] = 'vehicle'
+    df.loc[mask_vehicle, KEY_AGG] = 'vehicle'
     mask_traffic = df['readable'].str.lower().apply(lambda x: 'traffic' in x)
-    df.loc[mask_traffic, 'group_label'] = 'traffic'
+    df.loc[mask_traffic, KEY_AGG] = 'traffic'
     return df
 
 
@@ -60,9 +48,9 @@ def _split_construction_label(df):
     """
     df = df.copy()
     mask_barrier = df['name'].str.split('--').apply(lambda x: 'barrier' in x)
-    df.loc[mask_barrier, 'group_label'] = 'barrier'
+    df.loc[mask_barrier, KEY_AGG] = 'barrier'
     mask_flat = df['name'].str.split('--').apply(lambda x: 'flat' in x)
-    df.loc[mask_flat, 'group_label'] = 'flat'
+    df.loc[mask_flat, KEY_AGG] = 'flat'
     return df
 
 
@@ -87,89 +75,56 @@ def config_as_dataframe(config):
     df = pd.DataFrame(config['labels'])
     df['id'] = range(df.shape[0])
     df['family'] = df['name'].str.split('--').apply(lambda x: x[0])
-    df['group_label'] = df['family']
+    df[KEY_AGG] = df['family']
     df = _split_object_label(df)
     df = _split_construction_label(df)
     return df
 
 
-def resize(key, size, datadir):
-    """
-    :param key: string - image name, without its extension
-    :param size: integer - image size, in pixels
-    :param datadir: string - path to data repository
-    Returns
-    -------
-    Two resized images (train and labels)
-    """
-    train_fname = os.path.join(datadir, DATASET, 'training', 'images', key + '.jpg')
-    label_fname = os.path.join(datadir, DATASET, 'training', 'labels', key + '.png')
-    # open original images
-    train_img = Image.open(train_fname)
-    label_img = Image.open(label_fname)
+def aggregate_config(config, df):
+    """Aggregate the labels from the original configuration
 
-    # resize images (size*larger_size or larger_size*size)
-    img_in = utils.resize_image(train_img, size)
-    img_out = utils.resize_image(label_img, size)
-
-    # crop images to get size * size dimensions
-    crop_pix = np.random.randint(0, 1 + max(img_in.size) - size)
-    final_img_in = utils.mono_crop_image(img_in, crop_pix)
-    final_img_out = utils.mono_crop_image(img_out, crop_pix)
-    return final_img_in, final_img_out
-
-
-def group_image_label(image, df):
-    """Group the labels
-
-    :param image: PIL.Image
-    :param df: DataFrame - grouped labels
+    Parameters
+    ----------
+    config : dict
+    df : DataFrame
 
     Returns
     -------
-    PIL.Image
+    dict to serialize
     """
-    # turn all label ids into the lowest digits/label id according to its "group"
-    # (manually built)
-    a = np.array(image)
-    for label, grp in df.groupby("group_label")["id"]:
-        keep_label_id = min(grp)
-        for label_id in grp[1:]:
-            mask = a == label_id
-            a[mask] = keep_label_id
-    return Image.fromarray(a, mode=image.mode)
+    assert len(config['labels']) == df.shape[0], "should have the same size"
+    result = {'folder_structure': config['folder_structure']}
+    result['labels'] = []
+    for key, group in df.groupby(KEY_AGG):
+        label_id = int(group['id'].min())  # int conversion from int64 for JSON serialization
+        for _, label in group.iterrows():
+            if label['id'] == label_id:
+                d = config['labels'][label_id]
+                d['id'] = label_id
+                d['group_name'] = label[KEY_AGG]
+                d['aggregate'] = []
+            else:
+                d['aggregate'].append((label['id'], label['name']))
+        result['labels'].append(d)
+    return result
 
 
-def save_images(train_img, label_img, key, datadir, size):
-    """
-    :param train_img: PIL.Image - training image
-    :param label_img: PIL.Image - labelled training image
-    :param key: string - image name, without its extension
-    :param size: integer - image size, in pixels
-    :param datadir: string - path to data repository
-    """
-    train_dir = os.path.join(
-        datadir, DATASET, 'training_label_aggregate', 'images_' + str(size))
-    label_dir = os.path.join(
-        datadir, DATASET, 'training_label_aggregate', 'labels_' + str(size))
-    os.makedirs(train_dir, exist_ok=True)
-    os.makedirs(label_dir, exist_ok=True)
-    train_img.save(os.path.join(train_dir, key + '.jpg'))
-    label_img.save(os.path.join(label_dir, key + '.png'))
+def main(datadir):
+    """Generate a new config.json file with aggregated labels.
 
+    Parameters
+    ----------
+    datadir : str
 
-def main(key, size, datadir):
-    """
-    :param key: string - image name, without its extension
-    :param size: integer - image size, in pixels
-    :param datadir: string - path to data repository
+    Returns
+    -------
+    dict
     """
     config = read_config(datadir)
     df = config_as_dataframe(config)
-    df.to_csv(os.path.join(datadir, DATASET, 'config_agg_' + str(size) + '.csv'), index=False)
-    train_img, label_img = resize(key, size, datadir)
-    modified_label_img = group_image_label(label_img, df)
-    save_images(train_img, modified_label_img, key, datadir, size)
+    agg_config = aggregate_config(config, df)
+    return agg_config
 
 
 if __name__ == '__main__':
@@ -178,8 +133,11 @@ if __name__ == '__main__':
     parser.add_argument('-dp', '--datapath', required=False,
                         default="../data", nargs='?',
                         help="Relative path towards data directory")
-    parser.add_argument('-s', '--size', type=int, required=True,
-                        help="Desired size of images")
-    parser.add_argument('-k', '--key', required=True, help='The key/name of the image')
+    parser.add_argument('-s', '--save', required=False,
+                        default="config_aggregate.json",
+                        help="Name of the output JSON file.")
     args = parser.parse_args()
-    main(args.key, args.size, args.datapath)
+    label_aggregated = main(args.datapath)
+    with open(os.path.join(args.datapath, 'mapillary', args.save), 'w') as fobj:
+        print("write the file '{}'".format(os.path.join(args.datapath, 'mapillary', args.save)))
+        json.dump(label_aggregated, fobj)
