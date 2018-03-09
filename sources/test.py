bc26358c354ef336ba9081a95cf77554838d476f
Raphael Delhome <raphael.delhome@oslandia.com>
2018-03-09 15:12:32 +0100

    minor fixes about dataset label_ids property and get_nb_class method

--- sources/test.py
+++ sources/test.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
 
     # Glossary management (are all the labels required?)
     if args.label_list == -1:
-        label_list = list(testing_dataset.class_info.keys())
+        label_list = testing_dataset.label_ids
     else:
         label_list = args.label_list
         if sum([l>=testing_dataset.get_nb_class() for l in args.label_list]) > 0:
