6745a8a479e25dcda0d904fc5f04ee7c149c4d0d
Raphael Delhome <raphael.delhome@oslandia.com>
2018-03-22 12:11:45 +0100

    Clear obsolete methods in utils.py

--- sources/utils.py
+++ sources/utils.py
@@ -14,9 +14,6 @@ from PIL import Image
 import re
 import sys
 
-DATASET = ["training", "validation", "testing"]
-IMAGE_TYPES = ["images", "instances", "labels"]
-
 # Define the logger for the current project
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -28,30 +25,6 @@ ch_stdout.setFormatter(formatter)
 ch_logfile.setFormatter(formatter)
 logger.addHandler(ch_stdout)
 logger.addHandler(ch_logfile)
-
-def extract_features(data, pattern):
-    """Extract features from data that respect the given string pattern
-
-    Parameters
-    ----------
-    data: pd.DataFrame
-        dataframe to filter
-    pattern: object
-        string designing the column filter
-    """
-    return data[[col for col in data.columns if re.search(pattern, col) is not None]]
-
-def unnest(l):
-    """Unnest a list of lists, by splitting sublists and returning a simple list of scalar elements
-
-    Parameters
-    ----------
-    l: list
-        list of lists
-    
-    """
-    return [index for sublist in l for index in sublist]
-
 
 def compute_monotonic_weights(nb_images, label_counter, mu=0.5, max_weight=10):
     """Compute monotonic weights regarding the popularity of each label given
@@ -89,33 +62,6 @@ def compute_centered_weights(nb_images, label_counter, mu=0.5):
     """
     return [math.log(1 + mu * (l - nb_images / 2) ** 2 / nb_images)
             for l in label_counter]
-
-def mapillary_label_reading(labels):
-    """Gives the readable versions of Mapillary labels
-
-    Parameters
-    ----------
-    labels: dict
-        set of Mapillary labels, as defined in the Mapillary classification
-    
-    """
-    return [l['readable'] for l in labels]
-
-def one_hot_encoding(image_filename, nb_labels):
-    """Build a list of integer labels that are contained into a candidate
-    filtered image designed by its name on file system; the labels are
-    recognized starting from image pixels
-
-    Parameters
-    ----------
-    image_filename: object
-        File name of the image that has to be encoded
-    nb_labels: integer
-        number of labels contained into the reference classification
-
-    """
-    image = Image.open(image_filename)
-    return mapillary_label_building(image, nb_labels)
 
 def mapillary_label_building(filtered_image, label_ids):
     """Build a list of integer labels that are contained into a candidate
@@ -166,133 +112,6 @@ def mapillary_image_size_plot(data, filename):
     plt.tight_layout()
     plt.savefig(os.path.join("..", "images", filename))
 
-def size_inventory():
-    """Make an inventory of the size of every image in the dataset (training,
-    validation and testing sets taken together)
-
-    """
-    sizes = []
-    widths = []
-    heights = []
-    types = []
-    filenames = []
-    datasets = []
-    for dataset in DATASET:
-        logger.info("Size inventory in {} dataset".format(dataset))
-        for img_filename in os.listdir(os.path.join("..", "data", dataset, "images")):
-            for img_type in IMAGE_TYPES:
-                if dataset == "testing" and not img_type == "images":
-                    continue
-                complete_filename = os.path.join("..", "data", dataset,
-                                                 img_type, img_filename)
-                if not img_type == "images":
-                    complete_filename = complete_filename.replace("images", img_type)
-                    complete_filename = complete_filename.replace(".jpg", ".png")
-                image = Image.open(complete_filename)
-                datasets.append(dataset)
-                types.append(img_type)
-                filenames.append(complete_filename.split("/")[-1].split(".")[0])
-                sizes.append(image.size)
-                widths.append(image.size[0])
-                heights.append(image.size[1])
-    return pd.DataFrame({"dataset": datasets,
-                      "filename": filenames,
-                      "img_type": types,
-                      "size": sizes,
-                      "width": widths,
-                      "height": heights})
-
-def mapillary_data_preparation(datapath, dataset, size, nb_labels):
-    """Prepare the Mapillary dataset for processing convolutional neural network
-    computing: the images are resized and renamed as `index.jpg`, where `index`
-    is an integer comprised between 0 and 17999 (size of the validation
-    dataset), and the label are computed as integer lists starting from
-    Mapillary filtered images (as a remark, there are no label for the testing
-    data set)
-
-    Parameters
-    ----------
-    datapath: object
-        string designing the image dataset relative path
-    dataset: object
-        string designing the dataset type (either `training` or `validation`)
-    size: integer
-        desired image size (width and height are equals)
-    nb_labels: integer
-        number of label in the Mapillary classification
-
-    """
-    logger.info("Generating images from {} dataset...".format(dataset))
-    IMAGE_PATH = os.path.join(datapath, dataset, "images")
-    INPUT_PATH = os.path.join(datapath, dataset, "input_" + str(size))
-    LABEL_PATH = os.path.join(datapath, dataset, "labels")
-    OUTPUT_PATH = os.path.join(datapath, dataset, "output_" + str(size))
-    os.makedirs(INPUT_PATH, exists=True)
-    os.makedirs(OUTPUT_PATH, exists=True)
-    image_files = os.listdir(IMAGE_PATH)
-    train_label_descr = (["name", "raw_image",
-                          "old_width", "old_height", "pixel_ratio"] +
-                         ["label"+str(i) for i in range(nb_labels)])
-    train_labels = [pd.DataFrame(generate_images(img_id, img_fn, size, 10, nb_labels,
-                                                 IMAGE_PATH, INPUT_PATH,
-                                                 LABEL_PATH), columns=train_label_descr)
-                    for img_id, img_fn in enumerate(image_files)]
-    train_labels = pd.concat(train_labels)
-    train_labels = train_labels.sort_values(by="name")
-    train_labels.to_csv(os.path.join(OUTPUT_PATH, "labels.csv"),
-                        index=False)
-
-def generate_images(img_id, img_filename, base_size, nb_subimages, nb_labels,
-                    image_path, input_path, label_path):
-    """Generate a bunch of sub-images from another image by cropping operations
-
-    Parameter
-    ---------
-    img_id: integer
-        Raw image id, that will be integrated in the new image names
-    img_filename: object
-        String designing the name of the starting image
-    base_size: integer
-        Desired image size (width=height)
-    nb_sumimages: integer
-        Number of generated sub-images from each input image
-    nb_labels: integer
-        Total number of labels within the glossary
-    image_path: object
-        String designing the relative path to images
-    input_path: object
-        String designing the relative path in which new images must be saved
-    label_path: object
-        String designing the relative path to labelled images
-    """
-
-    logger.info(("Create images {} to {} from {}..."
-                 "").format(img_id*10, (img_id+1)*10-1, img_filename))
-    img_in = Image.open(os.path.join(image_path, img_filename))
-    img_out = Image.open(os.path.join(label_path,
-                                      img_filename.replace(".jpg", ".png")))
-    # Resizing
-    old_width, old_height = img_in.size
-    img_in = resize_image(img_in, base_size*2)
-    img_out = resize_image(img_out, base_size*2)
-    new_width, new_height = img_in.size
-    resizing_ratio = math.ceil(old_width * old_height / (new_width * new_height))
-    # Sub-image generation
-    labels = []
-    x = np.random.randint(0, img_in.size[0] - base_size, nb_subimages)
-    y = np.random.randint(0, img_in.size[1] - base_size, nb_subimages)
-    for i, x_, y_ in zip(range(len(x)), x, y):
-        new_img = crop_image(img_in, (x_, y_, x_ + base_size, y_ + base_size))
-        new_img = flip_image(new_img)
-        new_img_name = "{:05d}{}.jpg".format(img_id, i)
-        new_img.save(os.path.join(input_path, new_img_name))
-        labels_out = [new_img_name, img_filename, img_in.size[0],
-                      img_in.size[1], resizing_ratio]
-        new_img_out = img_out.crop((x_, y_, x_ + base_size, y_ + base_size))
-        labels_out = labels_out + mapillary_label_building(new_img_out, nb_labels)
-        labels.append(labels_out)
-    return labels
-
 def resize_image(img, base_size):
     """ Resize image `img` such that min(width, height)=base_size; keep image
     proportions
@@ -320,20 +139,6 @@ def mono_crop_image(img, crop_pixel):
     else:
         return img.crop((0, crop_pixel, img.width, crop_pixel+img.width))
 
-def crop_image(img, coordinates):
-    """ Crop image `img` following coordinates `coordinates`; simple overload
-    of the Image.crop() procedure
-
-    Parameters:
-    -----------
-    img: Image
-        input image to resize
-    coordinates: list/tuple
-        iterable object of 4 elements (left, up, right, down) that bound the
-    cropping process
-    """
-    return img.crop(coordinates)
-
 def flip_image(img, proba=0.5):
     """ Flip image `img` horizontally with a probability of `proba`
 
@@ -348,19 +153,6 @@ def flip_image(img, proba=0.5):
         return img
     else:
         return img.transpose(Image.FLIP_LEFT_RIGHT)
-
-def split_list(l, nb_partitions):
-    """Split a list `l` into `nb_partitions` equal sublists
-
-    Parameters
-    ----------
-    l: list
-        List of elements to split
-    nb_partitions: integer
-        number of desired partitions
-    """
-    return [l[i:(i+len(l)/nb_partitions)]
-            for i in range(0, len(l), int(len(l)/nb_partitions))]
 
 def list_to_str(seq, sep='-'):
     """Transform the input sequence into a ready-to-print string
