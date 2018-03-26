eb7f3ab255fbc60873207319f105c98332f1cfce
Raphael Delhome <raphael.delhome@oslandia.com>
2018-03-26 12:28:41 +0200

    Drop useless header and license from Python modules

--- sources/test.py
+++ sources/test.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""/**
- *   This script aims to test a neural network model in order to detect
- *   objects from street-scene (based on Mapillary Vistas dataset)
-
- *   Raphael Delhome - september 2017
- *
- *   This library is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU Library General Public
- *   License as published by the Free Software Foundation; either
- *   version 2 of the License, or (at your option) any later version.
- *   
- *   This library is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *   Library General Public License for more details.
- *   You should have received a copy of the GNU Library General Public
- *   License along with this library; if not, see <http://www.gnu.org/licenses/>
- */
-
-"""
 
 import argparse
 import os
