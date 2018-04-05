ca0a7b9a69331f111a9cde0d94c8fa907be24a55
Raphael Delhome <raphael.delhome@oslandia.com>
2018-04-05 17:29:17 +0200

    Add a unit test for to_categorical function

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,8 @@
 """Unit test related to the generator building and feeding
 """
 
+import numpy as np
+
 from deeposlandia import generator, network, utils
 
 def test_mapillary_generator():
@@ -36,3 +38,20 @@ def test_shape_generator():
     assert im_shape == (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3)
     label_shape = item[1].shape
     assert label_shape == (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, len(config['classes']))
+
+def test_to_categorical():
+    """Test `to_categorical` function in `generator` module:
+    - test if output shape is input shape + an additional dimension given by the `label_ids` length
+    - test if both representation provides the same information (native array on the first hand and
+    its one-hot version on the second hand)
+
+    """
+    MIN = 0
+    MAX = 10
+    SIZE = 3
+    a = np.random.randint(MIN, MAX, [SIZE, SIZE])
+    asum = np.bincount(a.reshape(-1))
+    b = generator.to_categorical(a, label_ids=range(MIN, MAX))
+    bsum = np.sum(b, axis=(0, 1))
+    assert b.shape == (SIZE, SIZE, MAX)
+    assert list(bsum) == list(asum)
