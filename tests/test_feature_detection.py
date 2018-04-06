2d2ded807630da2fed155d367161dbdb5c8ec407
Raphael Delhome <raphael.delhome@oslandia.com>
2018-04-06 17:57:11 +0200

    Add a unit test for VGG16-inspired feature detection network architecture

--- tests/test_feature_detection.py
+++ tests/test_feature_detection.py
@@ -5,7 +5,7 @@ from keras.models import Model
 
 from deeposlandia.keras_feature_detection import FeatureDetectionNetwork
 
-def test_network_instanciation():
+def test_simple_network_architecture():
     """Test the instanciation of a simple feature detection network
 
     """
@@ -14,6 +14,24 @@ def test_network_instanciation():
     NB_LABELS = 3
     net = FeatureDetectionNetwork("fd_test", image_size=IMAGE_SIZE,
                                   nb_channels=NB_CHANNELS, nb_labels=NB_LABELS)
+    m = Model(net.X, net.Y)
+    input_shape = m.input_shape
+    output_shape = m.output_shape
+    assert len(input_shape) == 4
+    assert input_shape[1:] == (IMAGE_SIZE, IMAGE_SIZE, NB_CHANNELS)
+    assert len(output_shape) == 2
+    assert output_shape[1] == NB_LABELS
+
+def test_vgg16_network_architecture():
+    """Test the instanciation of a VGG16-inspired feature detection network
+
+    """
+    IMAGE_SIZE = 224
+    NB_CHANNELS = 3
+    NB_LABELS = 3
+    net = FeatureDetectionNetwork("fd_test", image_size=IMAGE_SIZE,
+                                  nb_channels=NB_CHANNELS, nb_labels=NB_LABELS,
+                                  architecture="vgg16")
     m = Model(net.X, net.Y)
     input_shape = m.input_shape
     output_shape = m.output_shape
