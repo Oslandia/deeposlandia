fa7d0aa1b06b85942dbc76ec47e6ad97fb866c91
Raphael Delhome <raphael.delhome@oslandia.com>
2018-04-10 09:58:48 +0200

    Refactor the generator building, by implementing a basic build_generator function called by problem-specific generator builders

--- deeposlandia/generator.py
+++ deeposlandia/generator.py
@@ -33,11 +33,43 @@ def to_categorical(img, label_ids):
     output_shape = input_shape + (len(label_ids), )
     return categorical.reshape(output_shape)
 
-def create_generator(dataset, datapath, image_size, batch_size, config, seed=1337):
-    """Create a Keras data Generator starting from images contained in `datapath` repository
+def build_generator(datapath, gen_type, image_size, batch_size, seed=1337):
+    """Build a couple of generator
 
     Parameters
-    ---------- 
+    ----------
+    datapath : str
+        Path to image repository
+    gen_type : str
+        Generator type, either `images` or `labels`
+    image_size : integer
+        Number of width (resp. height) pixels
+    batch_size : integer
+        Number of images in each training batch
+    seed : integer
+        Random number generation for data shuffling and transformations
+
+    Returns
+    -------
+    generator
+        Input image generator
+    """
+    col_mode = 'grayscale' if gen_type == "labels" else 'rgb'
+    generator = ImageDataGenerator()
+    return generator.flow_from_directory(datapath,
+                                         classes=[gen_type],
+                                         target_size=(image_size, image_size),
+                                         batch_size=batch_size,
+                                         class_mode=None,
+                                         color_mode=col_mode,
+                                         seed=seed)
+
+def feature_detection_generator(dataset, datapath, image_size, batch_size, config, seed=1337):
+    """Create a Keras data Generator starting from images contained in `datapath` repository to
+    address the feature detection problem
+
+    Parameters
+    ----------
     dataset : str
         Name of the dataset
     datapath : str
@@ -55,29 +87,44 @@ def create_generator(dataset, datapath, image_size, batch_size, config, seed=133
     -------
     generator
         Generator of tuples (images, labels), for each input data batch
+
     """
-    generator = ImageDataGenerator()
-    im_generator = generator.flow_from_directory(
-        datapath,
-        classes=['images'],
-        target_size=(image_size, image_size),
-        batch_size=batch_size,
-        class_mode=None,
-        seed=seed)
-    label_generator = generator.flow_from_directory(
-        datapath,
-        classes=['labels'],
-        target_size=(image_size, image_size),
-        batch_size=batch_size,
-        class_mode=None,
-        color_mode='grayscale',
-        seed=seed)
+    image_generator = build_generator(datapath, "images", image_size, batch_size, seed)
+    label_generator = build_generator(datapath, "labels", image_size, batch_size, seed)
+    return zip(im_generator, label_generator)
+
+def semantic_segmentation_generator(dataset, datapath, image_size, batch_size, config, seed=1337):
+    """Create a Keras data Generator starting from images contained in `datapath` repository to
+    address the semantic segmentation problem
+
+    Parameters
+    ----------
+    dataset : str
+        Name of the dataset
+    datapath : str
+        Path to image repository
+    image_size : integer
+        Number of width (resp. height) pixels
+    batch_size : integer
+        Number of images in each training batch
+    config : dict
+        Dataset glossary
+    seed : integer
+        Random number generation for data shuffling and transformations
+
+    Returns
+    -------
+    generator
+        Generator of tuples (images, labels), for each input data batch
+
+    """
     if dataset == 'shapes':
         label_ids = [c for c in config['classes']]
         label_ids.sort()
     else:
         labels = config['labels']
         label_ids = [x['id'] for x in labels]
-    label_generator = iter(to_categorical(x, label_ids)
-                           for x in label_generator)
+    image_generator = build_generator(datapath, "images", image_size, batch_size, seed)
+    label_generator = build_generator(datapath, "labels", image_size, batch_size, seed)
+    label_generator = iter(to_categorical(x, label_ids) for x in label_generator)
     return zip(im_generator, label_generator)
