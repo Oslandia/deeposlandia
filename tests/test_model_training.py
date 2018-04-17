3d2f03014aac309da616c59d8b8540ba0c95a782
Raphael Delhome <raphael.delhome@oslandia.com>
2018-04-17 15:57:35 +0200

    tests: add a new test for verifying the neural network backup recovering
    When saved on the file system under the `.h5` format, neural network
    models can be recovered with `load_weights` method: applied to a
    model, it affects the saved weights to the current model object. This
    commit add a test function to control this behavior on a small
    example.

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -19,16 +19,16 @@ def test_model_training(shapes_image_size, shapes_sample, shapes_sample_config, 
     One big test function to avoid duplicating the training operations (that can be long)
     """
     BATCH_SIZE = 10
+    NB_EPOCHS = 1
     NB_STEPS = shapes_nb_images // BATCH_SIZE
     config = read_config(shapes_sample_config)
-    label_ids = [x['id'] for x in config["labels"]]
-
+    label_ids = [x['id'] for x in config["labels"] if x['is_evaluate']]
     gen = create_generator("shapes", "feature_detection", shapes_sample,
                            shapes_image_size, BATCH_SIZE, label_ids)
     cnn = FeatureDetectionNetwork("test", image_size=shapes_image_size, nb_labels=len(label_ids))
     model = Model(cnn.X, cnn.Y)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-    hist = model.fit_generator(gen, epochs=1, steps_per_epoch=NB_STEPS)
+    hist = model.fit_generator(gen, epochs=NB_EPOCHS, steps_per_epoch=NB_STEPS)
     assert len(hist.history) == 2
     assert all(k in hist.history.keys() for k in ['acc', 'loss'])
     assert hist.history['acc'][0] >= 0 and hist.history['acc'][0] <= 1
@@ -38,6 +38,31 @@ def test_model_training(shapes_image_size, shapes_sample, shapes_sample_config, 
     assert score.shape == (BATCH_SIZE, len(label_ids))
     assert all(0 <= s and s <= 1 for s in score.ravel())
 
-    BACKUP_FILENAME = os.path.join(str(shapes_temp_dir), "checkpoints", "test_model.h5")
+    BACKUP_FILENAME = os.path.join(str(shapes_temp_dir), "checkpoints", "test_model_{:02d}.h5".format(NB_EPOCHS))
     model.save(BACKUP_FILENAME)
     assert os.path.isfile(BACKUP_FILENAME)
+
+def test_model_backup_loading(shapes_image_size, shapes_sample_config, shapes_temp_dir):
+    """Test the model checkpoint recovering
+    """
+    config = read_config(shapes_sample_config)
+    label_ids = [x['id'] for x in config['labels'] if x['is_evaluate']]
+
+    cnn = FeatureDetectionNetwork("test", image_size=shapes_image_size, nb_labels=len(label_ids))
+    model = Model(cnn.X, cnn.Y)
+    old_weights = model.get_weights()
+    checkpoint_path = os.path.join(str(shapes_temp_dir), "checkpoints")
+    if os.path.isdir(checkpoint_path):
+        checkpoints = os.listdir(checkpoint_path)
+        if len(checkpoints) > 0:
+            model_checkpoint = checkpoints[-1]
+            trained_model_epoch = int(model_checkpoint[-5:-3])
+            checkpoint_complete_path = os.path.join(checkpoint_path, model_checkpoint)
+            model.load_weights(checkpoint_complete_path)
+    else:
+        trained_model_epoch = 0
+    new_weights = model.get_weights()
+    assert trained_model_epoch > 0
+    assert len(old_weights) == len(new_weights)
+    assert old_weights[0].shape == new_weights[0].shape
+    assert all(not np.allclose(old_weights[i], new_weights[i]) for i in range(len(new_weights)))
