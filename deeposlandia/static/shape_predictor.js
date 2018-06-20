b5d71f01faefa8850b43a9ec2ff96939b616948c
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-06-20 12:33:23 +0200

    app: Add some minor items in html webpages

--- deeposlandia/static/shape_predictor.js
+++ deeposlandia/static/shape_predictor.js
@@ -1,7 +1,7 @@
 // Load a new image from the static directory on click
 var predict_button = document.getElementById("predict_labels");
 predict_button.addEventListener("click", function(){
-  var image = document.getElementById("image");
+  var image = document.getElementById("raw_image");
 
   console.log("Generate a new image...");
   filename = generate_image(image);
@@ -15,7 +15,7 @@ predict_button.addEventListener("click", function(){
 function generate_image(image){
   if(image.complete){
     var new_image = new Image();
-    new_image.id = "image";
+    new_image.id = "raw_image";
     var image_id_str = "" + Math.floor(Math.random() * 5000 + 1);
     var image_id = ('00000'+image_id_str).substring(image_id_str.length);
     filename =  "/static/images/shape_".concat(image_id, ".png");
