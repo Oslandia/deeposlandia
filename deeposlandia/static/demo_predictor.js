c9bb61e6603c2b5631e84a65acaee57da711a210
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-06-21 09:34:15 +0200

    app: refactor the templates and static filenames

--- deeposlandia/static/demo_predictor.js
+++ deeposlandia/static/demo_predictor.js
@@ -1,34 +1,41 @@
 // Load a new image from the static directory on click
-var predict_button = document.getElementById("predict_labels");
-predict_button.addEventListener("click", function(){
-  var image = document.getElementById("raw_image");
+document.getElementById("predict_labels").addEventListener("click", function(){
 
-  console.log("Generate a new image...");
-  filename = generate_image(image);
   document.getElementById("result").innerHTML = "";
-  console.log(filename);
+  console.log("Randomly pick an image filename");
 
-  console.log("Predict labels for " + model);
-  predict_labels(filename, model);
+  get_image_name(dataset);
 });
 
-function generate_image(image){
+function get_image_name(dataset, filename_callback){
+  $.ajax({type: 'GET',
+	  url: '/mapillary_image_selector',
+	  data: {dataset: dataset},
+	  success: function(response){
+	    var filename = "/static/" + dataset + "/" + response.image_name;
+	    console.log("Generate a new image starting from file " + response.image_name + "...");
+	    generate_image(document.getElementById("raw_image"), filename);
+	    console.log("Predict labels for " + model);
+	    predict_labels(filename, dataset, model);
+	    console.log("Prediction OK!");  
+	  }
+	 });
+}
+  
+function generate_image(image, image_name){
   if(image.complete){
     var new_image = new Image();
     new_image.id = "raw_image";
-    var image_id_str = "" + Math.floor(Math.random() * 5000 + 1);
-    var image_id = ('00000'+image_id_str).substring(image_id_str.length);
-    filename =  "/static/images/shape_".concat(image_id, ".png");
-    new_image.src = filename
+    new_image.src = image_name
     image.parentNode.insertBefore(new_image, image);
     image.parentNode.removeChild(image);
   }
-  return filename
 };
 
-function predict_labels(filename, model){
-  $.getJSON('/shape_prediction', {
+function predict_labels(filename, dataset, model){
+  $.getJSON('/demo_prediction', {
     img: filename,
+    dataset: dataset,
     model: model
   }, function(data){
     var result = [];
--- deeposlandia/static/predictor.js
+++ deeposlandia/static/predictor.js
@@ -1,50 +1,36 @@
 // Load a new image from the static directory on click
 document.getElementById("predict_button").addEventListener("click", function(){
-  var uploaded_image = document.getElementById("uploaded_image");
 
-  console.log("Predict labels...");
   document.getElementById("result_label").innerHTML = "";
-
-  var filename = uploaded_image.src;
   console.log("Predict labels for " + model);
+  var uploaded_image = document.getElementById("uploaded_image");
+  var filename = uploaded_image.src;
   predict_labels(filename, "mapillary", model);
-
   console.log("Prediction OK!");
-  function predict_labels(filename, dataset, model){
 
+  function predict_labels(filename, dataset, model){
+    
     $.getJSON('/prediction', {
       img: filename,
       dataset: dataset,
       model: model
     }, function(data){
       var result = [];
-      if (model === "feature_detection") {
-	$.each(data, function(image, predictions){
-	  result.push("<ul>");
-	  $.each(predictions, function(key, val){
-      	    result.push("<li>" + key + ": " + val + "%</li>" );
-	  });
-	  result.push("</ul>");
-	});
-	document.getElementById("result_label").innerHTML = result.join("")
-      } else {
-	var predicted_image_path;
-	$.each(data.lab_images, function(image, predicted_image){
-	  predicted_image_path = "/static/predicted_images/" + predicted_image;
-	  result.push("<img id='predicted_image'><label>Predicted labels</label>");
-	});
-	result.push("<ul>");
-	$.each(data.labels, function(label, color){
-	  if (label != "background") {
-    	    result.push("<li><font color='" + color + "'>" + label + "</font></li>" );
-	  }
-	});
-	result.push("</ul>");
-	document.getElementById("result_label").innerHTML = result.join("");
-	document.getElementById("predicted_image").src = predicted_image_path;
-      }
-    });/*$.getJSON*/
-
+      var predicted_image_path;
+      $.each(data.lab_images, function(image, predicted_image){
+	predicted_image_path = "/static/predicted_images/" + predicted_image;
+	result.push("<img id='predicted_image'><label>Predicted labels</label>");
+      });
+      result.push("<ul>");
+      $.each(data.labels, function(label, color){
+	if (label != "background") {
+    	  result.push("<li><font color='" + color + "'>" + label + "</font></li>" );
+	}
+      });
+      result.push("</ul>");
+      document.getElementById("result_label").innerHTML = result.join("");
+      document.getElementById("predicted_image").src = predicted_image_path;
+    }); //$.getJSON
   };
 
 });
