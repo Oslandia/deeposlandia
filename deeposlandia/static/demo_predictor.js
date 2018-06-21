8cd160143cdad36bac16aabf5da13f8971045bec
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-06-21 17:51:16 +0200

    app: update javascript code to display ground truth information as well as predictions with accurate label colors

--- deeposlandia/static/demo_predictor.js
+++ deeposlandia/static/demo_predictor.js
@@ -12,21 +12,21 @@ function get_image_name(dataset, filename_callback){
 	  url: '/demo_image_selector',
 	  data: {dataset: dataset},
 	  success: function(response){
-	    var img_folder = "/static/" + dataset + "/images/";
-	    var lbl_folder = "/static/" + dataset + "/labels/";
-	    var img_filename = img_folder + response.image_name;
-	    if (dataset === "mapillary"){
-	      img_filename += ".jpg";
-	    } else {
-	      img_filename += ".png";
-	    }
-	    var lbl_filename = lbl_folder + response.image_name + ".png"
 	    console.log("File " + response.image_name + " will be used as demo...");
-	    generate_image("raw_image", img_filename);
-	    generate_image("ground_truth", lbl_filename);
+	    generate_image("raw_image", response.image_file);
+	    generate_image("ground_truth", response.label_file);
+	    var result = []
+	    result.push("<label>Ground-truth labels</label><ul>");
+	    $.each(response.labels, function(label, color){
+	      if (label != "background"){
+    		result.push("<li><font color='" + color + "'>" + label + "</font></li>" );
+	      }
+	    });
+	    result.push("</ul>");
+	    document.getElementById("ground_truth_labels").innerHTML = result.join("");
 	    console.log("Predict labels for " + model);
-	    predict_labels(img_filename, dataset, model);
-	    console.log("Prediction OK!");  
+	    predict_labels(response.image_file, dataset, model);
+	    console.log("Prediction OK!");
 	  }
 	 });
 }
@@ -52,15 +52,16 @@ function predict_labels(filename, dataset, model){
     if (model === "feature_detection") {
       $.each(data, function(image, predictions){
 	result.push("<ul>");
-	$.each(predictions, function(key, val){
-      	  result.push("<li>" + key + ": " + val + "%</li>" );
+	$.each(predictions, function(label, info){
+      	  result.push("<li><font color='" + info.color + "'>"
+		      + label + ": " + info.probability + "%</color></li>" );
 	});
 	result.push("</ul>");
       });
       document.getElementById("result").innerHTML = result.join("")
     } else {
       var predicted_image_path;
-      $.each(data.lab_images, function(image, predicted_image){
+      $.each(data.label_images, function(image, predicted_image){
 	predicted_image_path = "/static/predicted_images/" + predicted_image;
 	result.push("<img id='predicted_image'><label>Predicted labels</label>");
       });
