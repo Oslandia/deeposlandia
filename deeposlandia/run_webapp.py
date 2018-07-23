7a192eaabcbf36b25fdbdd66882694426a039084
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-07-23 17:34:15 +0200

    app: add some if clauses about config.ini before launching app

--- deeposlandia/run_webapp.py
+++ deeposlandia/run_webapp.py
@@ -3,14 +3,23 @@
 WARNING: only for development purpose!!
 """
 
-import os
 from configparser import ConfigParser
+import os
+import sys
 
 from deeposlandia import utils
 from deeposlandia.webapp import app
 
 config = ConfigParser()
-config.read("config.ini")
+CONFIG_FILENAME = "config.ini"
+if os.path.isfile(CONFIG_FILENAME):
+    config.read(CONFIG_FILENAME)
+else:
+    utils.logger.error("No file config.ini!")
+    sys.exit(1)
+if not "symlink" in config.sections():
+    utils.logger.error("Config.ini file does not contain any 'symlink' section.")
+    sys.exit(1)
 for link_name, path in config.items("symlink"):
     utils.create_symlink(os.path.join(app.static_folder, link_name), path)
 
