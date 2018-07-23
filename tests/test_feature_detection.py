6961059fd55bf353e285f65c685882889ff682a8
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-07-23 12:07:16 +0200

    tests: add new tests to verify new implemented architectures

--- tests/test_feature_detection.py
+++ tests/test_feature_detection.py
@@ -30,7 +30,24 @@ def test_vgg16_network_architecture(mapillary_image_size, nb_channels, mapillary
     """
     net = FeatureDetectionNetwork("fd_test", image_size=mapillary_image_size,
                                   nb_channels=nb_channels, nb_labels=mapillary_nb_labels,
-                                  architecture="vgg16")
+                                  architecture="vgg")
+    m = Model(net.X, net.Y)
+    input_shape = m.input_shape
+    output_shape = m.output_shape
+    assert len(input_shape) == 4
+    assert input_shape[1:] == (mapillary_image_size, mapillary_image_size, nb_channels)
+    assert len(output_shape) == 2
+    assert output_shape[1] == mapillary_nb_labels
+
+
+@keras_test
+def test_inception_network_architecture(mapillary_image_size, nb_channels, mapillary_nb_labels):
+    """Test a Inception-inspired feature detection network
+
+    """
+    net = FeatureDetectionNetwork("fd_test", image_size=mapillary_image_size,
+                                  nb_channels=nb_channels, nb_labels=mapillary_nb_labels,
+                                  architecture="inception")
     m = Model(net.X, net.Y)
     input_shape = m.input_shape
     output_shape = m.output_shape
