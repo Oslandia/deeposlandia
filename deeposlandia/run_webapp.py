077ecef40efecdf7f2bcfa0e4f6c5b9d76dc6e5c
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-07-23 17:20:56 +0200

    app: move config parsing in run_webapp.py

--- deeposlandia/run_webapp.py
+++ deeposlandia/run_webapp.py
@@ -3,7 +3,16 @@
 WARNING: only for development purpose!!
 """
 
+import os
+from configparser import ConfigParser
+
+from deeposlandia import utils
 from deeposlandia.webapp import app
+
+config = ConfigParser()
+config.read("config.ini")
+for link_name, path in config.items("symlink"):
+    utils.create_symlink(os.path.join(app.static_folder, link_name), path)
 
 app.config['DEBUG'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
