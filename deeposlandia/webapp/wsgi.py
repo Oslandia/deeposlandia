4a9e520a3e50d4b4758c1b2e2aecfece0d9a4bd5
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-10-08 10:00:23 +0200

    app: add missing material

---
+++ deeposlandia/webapp/wsgi.py
@@ -0,0 +1,42 @@
+from deeposlandia.webapp.main import app, config
+
+PREFIX = '/deeposlandia'
+app.config['APPLICATION_ROOT'] = PREFIX
+app.config['SECRET_KEY'] = 'D33p0s14ndIA!'
+
+class ReverseProxied:
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    inspired from http://flask.pocoo.org/snippets/35/
+
+    nginx example
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', PREFIX)
+        if script_name:
+            environ['SCRIPT_NAME'] = PREFIX
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
